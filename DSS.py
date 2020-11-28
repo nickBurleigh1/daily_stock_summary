430b3e4ea7a39289630f4486d3b01b8eafc82559
Nick Burleigh <nicholas.burleigh1@snhu.edu>
2020-11-28 19:33:44 +0000

    Added bubble sort algorithm to the DSS.py file

--- DSS.py
+++ DSS.py
@@ -39,6 +39,37 @@ def outstandingSharesBySector(sector):
     print outDoc
   return outDoc
 
+#added getallstocksmargin
+def getAllStocksMargin():
+  thisList = []
+  query = collection.find({'$all'}, {"Profit Margin"})
+  
+  for doc in query:
+    outDoc = json.dumps(doc, indent=4, default=json_util.default)
+    thisList.append(outDoc)
+    return thisList
+  
+  
+#added bubble sort
+def bubbleSort(obj_list):
+  has_swapped = True  
+  total_iteration = 0  
+  while(has_swapped):  
+    has_swapped = False  
+    for i in range(len(obj_list)-1):
+          for j in range(0,len(obj_list)-1-i,1):
+              if obj_list[j] > obj_list[j+1]:
+                  obj_list[j], obj_list[j + 1] = obj_list[j + 1], obj_list[j]
+                  has_swapped = True 
+
+  return obj_list[0]
+  
+#added search by highest profitMargin
+def getHighProfitMargin(profitMargin):
+  for doc in collection.find({"Profit Margin" : profitMargin}):
+      outDoc = json.dumps(doc, indent=4, default=json_util.default)
+      print outDoc
+      return outDoc
 
 
 def top5ByIndustry(industry):
@@ -50,9 +81,6 @@ def top5ByIndustry(industry):
     print outDoc
     return outDoc
     
- 
-
-
 
 
 
@@ -61,6 +89,7 @@ def get_user_choice():
     print("\n[1] Display 50-Day Simple Moving Average symbols between set range.")
     print("[2] Display Ticker symbols by Industry.")
     print("[3] Display Outstanding Shares by Sector grouped by Industry.")
+    print("[4] Return most profitable company.")
     print("[q] Quit.")
     
     choice = ''
@@ -75,9 +104,6 @@ def title_bar():
     
     
 def menu():
-  #avgRange(0.0463, 0.07)
-  #searchByIndustry("Medical Laboratories & Research")
-  #outstandingSharesBySector("Healthcare")
   title_bar()
   choice = ''
   
@@ -99,6 +125,12 @@ def menu():
     if choice == '3':
       sector = raw_input("\nEnter the sector you wish to view: ")
       outstandingSharesBySector(sector)
+       
+    if choice == '4':
+      objectList[] = getAllStocksMargin()
+      top_earnings = bubbleSort(objectlist)
+      print("The highest earnings are " + top_earnings)
+      
       
     if choice == 'q':
         print("\nExiting Daily Stock Summary")
